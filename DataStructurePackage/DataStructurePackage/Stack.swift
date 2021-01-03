7ef8ed7109317aa0f3f9be93557168574c5bd828
AJ <jamaliaeman@gmail.com>
2021-01-02 23:40:58 -0500

    - Stack Implementation - Stack Unit Tests

---
+++ DataStructurePackage/DataStructurePackage/Stack.swift
@@ -0,0 +1,50 @@
+//
+//  Stack.swift
+//  DataStructurePackage
+//
+//  Created by Aeman Jamali on 1/2/21.
+//
+
+struct Stack<Element: Equatable>: Equatable {
+    // Step 1: Store
+    var storage: [Element] = []
+    var isEmpty: Bool {
+        // when can also use storage.isEmpty
+        return peek() == nil
+    }
+    
+    // helps to create empty stack
+    init(){}
+    
+    init(_ storage: [Element]){
+        self.storage = storage
+    }
+    
+    // Step 2: Push
+    mutating func push(_ element: Element) {
+        storage.append(element)
+    }
+    
+    // Step 3: Pop
+    @discardableResult
+    mutating func pop() -> Element? {
+        return storage.popLast()
+    }
+    
+    func peek() -> Element? {
+        return storage.last
+    }
+}
+
+extension Stack: CustomStringConvertible {
+    var description: String {
+        return storage.map { "\($0)"}
+            .joined(separator: " ")
+    }
+}
+
+extension Stack: ExpressibleByArrayLiteral {
+    init(arrayLiteral elements: Element...) {
+        storage = elements
+    }
+}
---
+++ DataStructurePackage/DataStructurePackageTests/StackTest.swift
@@ -0,0 +1,50 @@
+//
+//  StackTest.swift
+//  DataStructurePackageTests
+//
+//  Created by Aeman Jamali on 1/2/21.
+//
+
+import XCTest
+@testable import DataStructurePackage
+
+class StackTest: XCTestCase {
+    var stack = Stack<Int>()
+    
+    override func setUp() {
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
+        stack.push(4)
+    }
+    
+    func test_stack_description_matches() {
+        XCTAssertEqual(stack.description, "1 2 3 4")
+    }
+    
+    func test_pop() {
+        XCTAssertEqual(stack.pop(), 4)
+    }
+    
+    func test_peek() {
+        XCTAssertEqual(stack.peek(), 4)
+    }
+    
+    func test_stack_isEmpty() {
+        XCTAssertFalse(stack.isEmpty)
+    }
+    
+    func test_stack_init_withArray() {
+        let array = [1, 2, 3, 4]
+        
+        XCTAssertEqual(stack, Stack(array))
+    }
+    
+    func test_stack_arrayLiteral() {
+        let stack: Stack = ["Mango", "Avcado", "Apple"]
+        
+        XCTAssertEqual(stack, ["Mango", "Avcado", "Apple"])
+    }
+}
+
+
